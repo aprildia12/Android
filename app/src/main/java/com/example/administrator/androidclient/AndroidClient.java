6c574cac2cd928c0fb355b55ba8d1c10160b9334
aprildia12 <aprildia12@hanmail.net>
2016-06-15 17:27:09 +0900

    DB 수정중

--- app/src/main/java/com/example/administrator/androidclient/AndroidClient.java
+++ app/src/main/java/com/example/administrator/androidclient/AndroidClient.java
@@ -1,7 +1,14 @@
 package com.example.administrator.androidclient;
 
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import java.io.ObjectInputStream;
@@ -9,36 +16,145 @@ import java.io.ObjectOutputStream;
 import java.net.Socket;
 
 public class AndroidClient extends AppCompatActivity {
+    TextView textView;
+    EditText editText;  //
+    EditText editText2;
+
+    //
+    String databaseName;
+    SQLiteDatabase database;
+    String tableName;
+
+    android.os.Handler handler = new android.os.Handler();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        try{
-            String hostname = "192.168.123.101";    // 안드로이드 어플리케이션에서 사용하는 IP
-            int port = 9999;
+        textView = (TextView) findViewById(R.id.textView);
+        editText = (EditText) findViewById(R.id.editText);  //
+        editText2 = (EditText) findViewById(R.id.editText2);
+    }
 
-            Socket sock = new Socket(hostname, port);   // 소켓 연결을 위해 Socket 객체를 생성
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
 
-            // 데이터를 쓰기 위한 스트림 객체 생성. 그리고 쓰기
-            ObjectOutputStream outstream = new ObjectOutputStream( sock.getOutputStream() );
-            outstream.writeObject("Hello Android! ");
-            outstream.flush();
+        //getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
 
-            // 데이터를 읽기 위한 스트림 객체 생성. 그리고 쓰기.
-            ObjectInputStream instream = new ObjectInputStream( sock.getInputStream() );
-            String obj = (String) instream.readObject();
+    public void onButton1Clicked(View v) {  // onButtonClicked
+        ConnectThread thread = new ConnectThread();
+        thread.start();
+        //
+        databaseName = editText.getText().toString();
+        try {
+            database = openOrCreateDatabase(databaseName, Context.MODE_PRIVATE, null);
+            println("데이터 베이스를 열었습니다. : " + databaseName);
+        } catch(Exception e) { e.printStackTrace(); }
+    }
 
-            //activity_main.xml에 있는 text 뷰에 표시
-            TextView tText = new TextView(this);
-            tText.setText(obj);
-            setContentView(tText);
+    public void onButton2Clicked(View v) {
+        tableName = editText2.getText().toString();
 
-            sock.close();
-        }
-        catch(Exception ex) {
-            ex.printStackTrace();
+        try {
+            if (database != null) {
+                database.execSQL("CREAT TABLE if not exists " + tableName + "(" +
+                "_id integer PRIMARY KEY autoincrement, " + "name text, " +
+                "age integer," + "mobile text" + ")");
+                println("테이블을 만들었습니다. : " + tableName);
+            } else
+                println("데이터베이스를 먼저 열어야 합니다.");
+        } catch(Exception e) { e.printStackTrace(); }
+    }
+
+    public void onButton3Clicked(View v) {
+       try {
+           if (tableName == null) {
+               tableName = editText2.getText().toString();
+           }
+           if (database != null) {
+               database.execSQL("INSERT INTO " + tableName + "(name, age, mobile) VALUES" +
+               "('김가은' , 24, '010-1234-5678)");
+               println("데이터를 추가했습니다.");
+           } else
+               println("데이터베이스를 먼저 열어야 합니다.");
+       } catch(Exception e) { e.printStackTrace(); }
+    }
+
+    public void onButton4Clicked(View v) {
+        try {
+            if(tableName == null)
+                tableName = editText2.getText().toString();
+            if(database != null) {
+                Cursor cursor = database.rawQuery("SELECT name, age, mobile FROM " + tableName, null);
+
+                int count = cursor.getCount();
+                println("결과 레코드의 갯수 : " + count);
+
+                for(int i = 0; i < count; i++) {
+                    cursor.moveToNext();
+                    String name = cursor.getString(0);
+                    int age = cursor.getInt(1);
+                    String mobile = cursor.getString(2);
+
+                    println("레코드 #" + i + " : " + name + ", " + age + ", " + mobile);
+                }
+            } else
+                println("데이터베이스를 먼저 열어야 합니다.");
+        } catch(Exception e) { e.printStackTrace(); }
+    }
+
+    class ConnectThread extends Thread {
+        public void run() {
+
+            String host = "192.168.123.101";
+            int port = 5001;
+
+            try {
+                Socket socket = new Socket(host, port);
+                System.out.println("서버로 연결되었습니다." + host + ", " + port);
+
+                String output = "Hello";
+                ObjectOutputStream outstream = new ObjectOutputStream(socket.getOutputStream());
+                outstream.writeObject(output);
+                outstream.flush();
+                System.out.println("서버로 보낸 데이터: " + output);
+
+                ObjectInputStream instream = new ObjectInputStream(socket.getInputStream());
+                Object input = instream.readObject();
+                System.out.println("서버로부터 받은 데이터: " + input);
+
+                instream.close();
+                outstream.close();
+                instream.close();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
         }
     }
+    private void println(final String data){
+        handler.post(new Runnable() {
+            @Override
+            public void run() {
+                textView.append(data + "\n");
+            }
+        });
+        textView.append(data + "\n");   //
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+
+        int id = item.getItemId();
+
+        /*if (id == R.id.action_settings) {
+            return true;
+        }*/
+
+        return super.onOptionsItemSelected(item);
+    }
 }
+
